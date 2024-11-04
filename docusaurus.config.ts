4244e67e0cfe564ba34126b8d931967c46138a90
Kevin Lacker <lacker@gmail.com>
2024-11-04 15:07:33 -0800

    ok

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,63 +1,55 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'The Acorn Theorem Prover',
-  tagline: 'Perfectly rigorous mathematics, as simple as possible',
-  favicon: 'img/favicon.ico',
+  title: "The Acorn Theorem Prover",
+  tagline: "Perfectly rigorous mathematics, as simple as possible.",
+  favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: 'https://acornprover.org',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: "https://acornprover.org",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'acornprover', // Usually your GitHub org/user name.
-  projectName: 'acornprover.org', // Usually your repo name.
+  organizationName: "acornprover", // GitHub org name
+  projectName: "acornprover.org", // repo name
 
   trailingSlash: true,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
+          sidebarPath: "./sidebars.ts",
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/acornprover/acornprover.org/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/acornprover/acornprover.org/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
-          showReadingTime: true,
+          showReadingTime: false,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/acornprover/acornprover.org/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/acornprover/acornprover.org/tree/main/packages/create-docusaurus/templates/shared/",
+
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -65,59 +57,59 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'My Site',
+      title: "Acorn",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "Acorn Logo",
+        src: "img/achy64.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/acornprover/acornprover.org',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/acornprover/acornprover.org",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/acornprover',
+              label: "Twitter",
+              href: "https://twitter.com/acornprover",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/acornprover',
+              label: "GitHub",
+              href: "https://github.com/acornprover",
             },
           ],
         },
